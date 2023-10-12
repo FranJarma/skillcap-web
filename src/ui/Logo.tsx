41601cb7d54dd289c0b99990081c8f6602cf914f
Francisco Jarma <francisco.jarma@deltatre.com>
2023-10-12 14:22:42 -0300

    SC-01: First commit

---
+++ src/ui/Logo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+import LogoWhite from '*.png';
+export const Logo = () => (
+    <Image
+    src="/vercel.svg"
+    alt="Logo Skillcap"/>
+);
---
+++ src/ui/index.ts
@@ -0,0 +1,5 @@
+export * from './CTA';
+export * from './Footer';
+export * from './Header';
+export * from './Logo';
+export * from './Sidebar';
